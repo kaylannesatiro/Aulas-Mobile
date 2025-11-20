2ed7e1e99f99ee074136e9d8e5cc632af8ffac31
Kaylanne <kaylannesatiro@icloud.com>
2025-11-19 23:03:22 -0300

    feat: Aplicação Simples para calcular IMC

--- intro-react-native/App.js
+++ intro-react-native/App.js
@@ -1,7 +1,8 @@
-import HelloWorld from "./components/aula-01/HelloWorld";
+//import HelloWorld from "./components/aula-01/HelloWorld";
+import MeuIMC from "./components/aula-02/MeuIMC";
 
 export default function App() {
   return (
-    <HelloWorld />
+    <MeuIMC/>
   );
 };
---
+++ intro-react-native/components/aula-02/MeuIMC.jsx
@@ -0,0 +1,65 @@
+import {View, Text, TextInput, Button} from "react-native";
+import { use, useState } from "react"; //gancho pro estado do componente, um código de fora do componente
+import estilos from "./estilos";
+
+const MeuIMC = () => {
+
+    const [peso, setPeso] = useState(0);
+    const [altura, setAltura] = useState(0);
+    const [resultado, setResultado] = useState(0);
+
+    function calcularIMC() {
+        setResultado (peso / altura**2);
+    }
+
+    return (
+        <View style={estilos.container}>
+            <View style={estilos.header}>
+                <Text style={estilos.texto}>
+                    Calculadora IMC
+                </Text>
+            </View>
+
+            <View style={estilos.form}>
+                <View style={estilos.formGroup}>
+                    <Text style={estilos.texto}>
+                        Peso:
+                    </Text>
+                    <TextInput 
+                        style={estilos.textoInput}
+                        keyboardType="numeric" //deixa o teclado numerico
+                        placeholder="Digite seu peso"
+                        onChangeText = {(x) => setPeso(x)}
+                    />
+                </View>
+
+                <View style={estilos.formGroup}>
+                    <Text style={estilos.texto}>
+                        Altura:
+                    </Text>
+                    <TextInput 
+                        style={estilos.textoInput}
+                        keyboardType="numeric" //deixa o teclado numerico
+                        placeholder="Digite sua altura"
+                        onChangeText = {(x) => setAltura(x)}
+                    />
+                </View>
+            </View>
+
+            <View>
+                <Button 
+                    title="CALCULAR"
+                    onPress={() => calcularIMC()}
+                />
+            </View>
+
+            <View style={estilos.form}>
+                <Text style={estilos.texto}>
+                    Resultado: {resultado}
+                </Text>
+            </View>
+        </View>
+    )
+}
+
+export default MeuIMC;
